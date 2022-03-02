5b965cb8f1bed1dc4958cb17f723cdd46762060b
Rob Petrin <robpetrin@gmail.com>
2022-03-02 12:55:34 -0500

    Fix for new links

--- assets/js/app.js
+++ assets/js/app.js
@@ -144,3 +144,13 @@ window.onload = () => {
     }
   }
 }
+
+// Force new links into a new window
+
+function externalLinks() {
+  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
+     var b = c[a];
+     b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
+  }
+};
+externalLinks();
